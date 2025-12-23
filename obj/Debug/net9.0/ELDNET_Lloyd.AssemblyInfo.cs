4607fdbb671f4ecaa73df975d44ac3fb15304aff
JarmenKell7688 <villahermosa.jr2017@gmail.com>
2025-12-23 10:37:01 +0800

    Breakthrough 1.1
    color change sa student UI from black to orange

--- obj/Debug/net9.0/ELDNET_Lloyd.AssemblyInfo.cs
+++ obj/Debug/net9.0/ELDNET_Lloyd.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ELDNET_Lloyd")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+08a53d222fa1d4f980a1dd645dc2660383f3476e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ELDNET_Lloyd")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ELDNET_Lloyd")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
